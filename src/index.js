9341987ccf0367468fbca3cea9fdc715bc780295
Jorgen Evens <jorgen@evens.eu>
2020-04-28 17:01:45 +0200

    [build] add browser build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const _fetch = require('@ambassify/fetch');
 const https = require('https');
 const http = require('http');
+
+const FetchQueue = require('./core');
 
 function createAgent(protocol, size) {
     return new protocol.Agent({
@@ -11,28 +12,13 @@ function createAgent(protocol, size) {
 }
 
 module.exports =
-class FetchQueue {
-
-    constructor(size, options = {}) {
-        this._queue = [];
-        this._pending = 0;
-        this._fetch = options.fetch || _fetch;
-
-        this.fetch = this.fetch.bind(this);
-        this._finish = this._finish.bind(this);
-        this._error = this._error.bind(this);
-
-        this.resize(size);
-    }
+class NodeFetchQueue extends FetchQueue {
 
     resize(size) {
-        this.size = size;
-        this._size = size; // @deprecated
-        this._httpsAgent = createAgent(https, this.size);
-        this._httpAgent = createAgent(http, this.size);
+        this._httpsAgent = createAgent(https, size);
+        this._httpAgent = createAgent(http, size);
 
-        // Trigger queued items if queue became bigger
-        this._check();
+        return super.resize(size);
     }
 
     fetch(input, init) {
@@ -43,49 +29,7 @@ class FetchQueue {
         init = init || {};
         init.agent = init.agent || agent;
 
-        return new Promise(resolve => this._push(resolve))
-            .then(() => this._fetch(input, init))
-            .then(this._finish)
-            .catch(this._error);
-    }
-
-    _check() {
-        if (this._pending >= this.size)
-            return;
-
-        if (this._queue.length < 1)
-            return;
-
-        this._pending++;
-        const start = this._queue.shift();
-        start();
-
-        // Flush all queued items until queue is full
-        this._check();
-    }
-
-    _push(start) {
-        this._queue.push(start);
-        this._check();
-    }
-
-    _pop() {
-        this._pending--;
-
-        if (this._pending < 0)
-            throw new Error('Pop called more than there were pending fetches');
-
-        this._check();
-    }
-
-    _finish(r) {
-        this._pop();
-        return r;
-    }
-
-    _error(err) {
-        this._pop();
-        throw err;
+        return super.fetch(input, init);
     }
 
 }
